9075c154a431b8b816835d7a0c4457277a4be6c5
Ellin <1833921+Ellin@users.noreply.github.com>
2026-01-20 10:56:16 +0000

    Create function that generates a grid of pixels
    A "pixel" is a square div. The grid layout is achieved using CSS flexbox.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 "use strict"
+
+const container = document.querySelector('.container');
+
+function generatePixels(gridDimension) {
+    for (let i = 0; i < gridDimension ** 2; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.style.width = ((1/gridDimension) * 100) + '%';
+        container.append(pixel);
+    }
+}
+
+generatePixels(16);
